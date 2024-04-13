e266f01f5f24d1a3f98199a7f1980046f1492dfa
bernardo.vitorino <bernardo.rvitorino@decsis.pt>
2024-04-13 22:13:18 +0100

    full of errors

--- DonkeySponsorBackend/src/modules/business/sponsoring/models.py
+++ DonkeySponsorBackend/src/modules/business/sponsoring/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 import uuid
 
-
-
 class Animal(models.Model):
     id = models.UUIDField( primary_key=True,
                             unique=True,
@@ -16,3 +14,51 @@ class Animal(models.Model):
                                 related_name='animal',
                                 on_delete=models.CASCADE)
     
+class AnimalImage(models.Model):
+    id = models.UUIDField( primary_key=True,
+                            unique=True,
+                            default=uuid.uuid4,
+                            editable=False)
+    
+    animal = models.ForeignKey('sponsoring.Animal',
+                                related_name='animal',
+                                on_delete=models.CASCADE)
+    
+    image = models.ImageField()
+
+class AnimalActivity(models.Model):
+    id = models.UUIDField( primary_key=True,
+                           unique=True,
+                           default=uuid.uuid4,
+                           editable=False)
+    
+    animal = models.ForeignKey('sponsoring.Animal',
+                                related_name='animal',
+                                on_delete=models.CASCADE)
+    
+    activity = models.ForeignKey('Activity',
+                                related_name='activity',
+                                on_delete=models.CASCADE)
+    
+class Activity(models.Model):
+    id = models.UUIDField( primary_key=True,
+                           unique=True,
+                           default=uuid.uuid4,
+                           editable=False) 
+    
+    description = models.CharField()
+    status = models.BooleanField()
+    lastUpdatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now=True)
+
+class ActivityImage(models.Model):
+    id = models.UUIDField( primary_key=True,
+                           unique=True,
+                           default=uuid.uuid4,
+                           editable=False) 
+    
+    activity = models.ForeignKey('sponsoring.Activity',
+                            related_name='activity',
+                            on_delete=models.CASCADE)
+    
+    image = models.ImageField()
