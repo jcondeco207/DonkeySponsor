927afcc1fde17e80df2c6157502bebd96c04beeb
bernardo.vitorino <bernardo.rvitorino@decsis.pt>
2024-04-20 16:55:42 +0100

    home page logic

--- DonkeySponsorBackend/src/frontend/src/App.tsx
+++ DonkeySponsorBackend/src/frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import DonkeyMenu from "./pages/components/DonkeyMenu";
+import {
+  Sticky,
+} from 'semantic-ui-react'
 
 export default function App() {
 
   return (
     <div className='app'>
       <div className='workArea'>
-        <DonkeyMenu />
+        <Sticky>
+          <DonkeyMenu />
+        </Sticky>
         <div className='contentArea'>
           <Outlet />
         </div>
--- DonkeySponsorBackend/src/frontend/src/pages/Farms.tsx
+++ DonkeySponsorBackend/src/frontend/src/pages/Farms.tsx
@@ -1,6 +1,96 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+import { 
+    Visibility,
+    ItemImage,
+    ItemHeader,
+    ItemGroup,
+    ItemExtra,
+    ItemDescription,
+    ItemContent,
+    Icon,
+    Image,
+    Item,
+ } from 'semantic-ui-react'
+
+type Activity = {
+    id: string,
+    description: string,
+    status: boolean,
+    lastUptadedAt: string,
+    createdAt: string,
+    activity_image: string,
+    donkey_id: string
+}
+
 export default function Farms() {
+
+    const [page, setPage] = useState<Activity[]>([])
+    const [numberOfPageLoads, setNumberOfPageLoads] = useState(2);
+
+    useEffect(() => {
+        const csrfToken = Cookies.get("csrftoken");
+        axios
+            .get(`/api/activities/`, {
+                headers: {
+                    Accept: "application/json",
+                    "X-CSRFToken": String(csrfToken)
+                },
+                params: {
+                    limit: 10
+                }
+            })
+            .then((response) => {
+                setPage(response.data.results);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const loadMoreContent = () => {
+        var activities = page;
+        const csrfToken = Cookies.get("csrftoken");
+
+        axios
+            .get(`/api/activities/`, {
+                headers: {
+                    Accept: "application/json",
+                    "X-CSRFToken": String(csrfToken)
+                },
+                params: {
+                    limit: 10,
+                    offset: numberOfPageLoads * 10
+                }
+            })
+            .then((response) => {
+                activities.push(response.data.results);
+                setPage(activities);
+                setNumberOfPageLoads(numberOfPageLoads + 1);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
+            <Visibility
+                fireOnMount
+                onBottomVisible={loadMoreContent}
+            >
+                <ItemGroup>
+                    {page.map((activity, index) => (
+                        <Item>
+                            
+                        </Item>
+                    ))}
+                </ItemGroup>
+
+
+            </Visibility>
         </div>
-    );    
+    );
 }
